877c4cee71eedc160153933c3390ff6602486282
JackAttack-365 <142643773+JackAttack365@users.noreply.github.com>
2024-09-25 19:13:43 -0700

    optimization

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/Lift.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/Lift.java
@@ -5,12 +5,11 @@ import androidx.annotation.NonNull;
 import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
 import com.acmerobotics.roadrunner.Action;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.teamcode.Config;
 import org.firstinspires.ftc.teamcode.enums.LiftPosition;
 import org.piedmontpioneers.intothedeep.enums.Color;
-import org.piedmontpioneers.intothedeep.utils.PIDFCoefficients;
+import org.piedmontpioneers.intothedeep.utils.PIDController;
 
 public class Lift extends SubSystem {
     private DcMotor lift;
@@ -25,7 +24,7 @@ public class Lift extends SubSystem {
 
     private LiftPosition position;
 
-    private PIDFCoefficients liftPID = new PIDFCoefficients(
+    private PIDController liftPID = new PIDController(
             0.9,
             0.1,
             0.9);
@@ -73,46 +72,57 @@ public class Lift extends SubSystem {
         config.telemetry.addData("Lift Busy?", lift.isBusy());
     }
 
-    public class LiftToTop implements Action {
+    public class LiftToTopBasket implements Action {
         private boolean initialized = false;
-        private ElapsedTime timer = new ElapsedTime();
-        double integralSum = 0;
-        double lastError = 0;
 
         @Override
         public boolean run(@NonNull TelemetryPacket telemetryPacket) {
-            double error;
-            double derivative;
-
             if (LIFT_TOP_BASKET + 40 >= lift.getCurrentPosition() && lift.getCurrentPosition() >= LIFT_TOP_BASKET) {
                 return true;
             } else {
-                // calculate the error
-                error = LIFT_TOP_BASKET - lift.getCurrentPosition();
-
-                // rate of change of the error
-                derivative = (error - lastError) / timer.seconds();
-
-                if (Math.abs(error) < 100) {
-                    integralSum += error * timer.seconds();
-                }
-
-                // PID formula
-                double outputPower = (liftPID.Kp * error) + (liftPID.Ki * integralSum) + (liftPID.Kd * derivative);
-
-                lift.setPower(Math.min(Math.max(outputPower, -1), 1));
-
-                lastError = error;
-
-                // reset the timer for next time
-                timer.reset();
+                lift.setPower(liftPID.pidToPos(lift, LIFT_TOP_BASKET));
                 return false;
             }
-
         }
     }
 
-    public Action liftToTop() {
-        return new LiftToTop();
+    public Action liftToTopBasket() {
+        return new LiftToTopBasket();
+    }
+
+    public class LiftToBottom implements Action {
+        private boolean initialized = false;
+
+        @Override
+        public boolean run(@NonNull TelemetryPacket telemetryPacket) {
+            if (LIFT_BOTTOM + 40 >= lift.getCurrentPosition() && lift.getCurrentPosition() >= LIFT_BOTTOM) {
+                return true;
+            } else {
+                lift.setPower(liftPID.pidToPos(lift, LIFT_BOTTOM));
+                return false;
+            }
+        }
+    }
+
+    public Action liftToBottom() {
+        return new LiftToBottom();
+    }
+
+    public class LiftToBottomBasket implements Action {
+        private boolean initialized = false;
+
+        @Override
+        public boolean run(@NonNull TelemetryPacket telemetryPacket) {
+            if (LIFT_BOTTOM_BASKET + 40 >= lift.getCurrentPosition() && lift.getCurrentPosition() >= LIFT_BOTTOM_BASKET) {
+                return true;
+            } else {
+                lift.setPower(liftPID.pidToPos(lift, LIFT_BOTTOM_BASKET));
+                return false;
+            }
+        }
+    }
+
+    public Action liftToBottomBasket() {
+        return new LiftToBottomBasket();
     }
 }
