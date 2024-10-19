4a49191da99557b147d8666997318fd6ccd2e281
JackAttack-365 <142643773+JackAttack365@users.noreply.github.com>
2024-10-18 17:10:35 -0700

    practice practice practice

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/EncoderPositionsTuner.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/EncoderPositionsTuner.java
@@ -9,49 +9,34 @@ import com.qualcomm.robotcore.hardware.Servo;
 @TeleOp(name="EncoderPositionsTuner", group="Tuning")
 public class EncoderPositionsTuner extends OpMode {
     public DcMotor lift;
-    public DcMotor extendo;
-    public Servo intakeServo;
-    public Servo outtakeServo;
 
+    public double speed = 0.5;
     @Override
     public void init() {
         lift = hardwareMap.get(DcMotor.class, "lift");
-        extendo = hardwareMap.get(DcMotor.class, "extendo");
 
-        intakeServo = hardwareMap.get(Servo.class,"intake");
-        outtakeServo = hardwareMap.get(Servo.class,"outtake");
+        lift.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        lift.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
-        //lift.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        extendo.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-
-        extendo.setDirection(DcMotorSimple.Direction.REVERSE);
-
-        intakeServo.setPosition(0);
+        lift.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
     }
 
     @Override
     public void loop() {
-        if (gamepad1.a) {
-            intakeServo.setPosition(0.18);
-        }
 
-        if (gamepad1.b) {
-            intakeServo.setPosition(0.33);
-        }
-
-        if (gamepad1.right_trigger >= 0.1) {
-            lift.setPower(1);
-            telemetry.addData("hi","f");
-        } if (gamepad1.left_trigger >= 0.1) {
-            lift.setPower(-0.3);
-            telemetry.addData("hi5","f34");
-        }else {
+        if (gamepad1.left_trigger >= 0.1) {
+            lift.setPower(speed);
+        } else if (gamepad1.right_trigger >= 0.1) {
+            lift.setPower(-speed);
+        } else {
             lift.setPower(0);
         }
+
+        if (gamepad1.a) {
+            speed = (speed == 0.5)?1:0.5;
+        }
         telemetry.addData("lift", lift.getCurrentPosition());
-        telemetry.addData("extendo", extendo.getCurrentPosition());
-        telemetry.addData("intake", intakeServo.getPosition());
-        telemetry.addData("outtake", outtakeServo.getPosition());
+        telemetry.addData("speed", speed);
         telemetry.update();
     }
 }
