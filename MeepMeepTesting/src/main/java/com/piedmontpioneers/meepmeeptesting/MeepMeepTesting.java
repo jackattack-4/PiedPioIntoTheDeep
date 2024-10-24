d732da89abd7c43d31e6aa4bd48109cdeaba181b
JackAttack-365 <142643773+JackAttack365@users.noreply.github.com>
2024-10-23 18:47:19 -0700

    Update MeepMeepTesting.java

--- MeepMeepTesting/src/main/java/com/piedmontpioneers/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/piedmontpioneers/meepmeeptesting/MeepMeepTesting.java
@@ -22,45 +22,23 @@ public class MeepMeepTesting {
                 .setConstraints(100, 100, Math.toRadians(180), Math.toRadians(180), 15)
                 .build();
 
-        RoadRunnerBotEntity blueDrive = new DefaultBotBuilder(meepMeep)
-                .setColorScheme(new ColorSchemeBlueDark())
-                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
-                .setConstraints(100, 100, Math.toRadians(180), Math.toRadians(180), 15)
-                .build();
-
-        redDrive.runAction(redDrive.getDrive().actionBuilder(new Pose2d(-13, -59, Math.toRadians(90)))
-                .strafeTo(new Vector2d(-8, -34))
-                .splineToLinearHeading(new Pose2d(-48, -35, Math.toRadians(270)),2)
-                .waitSeconds(INTAKE)
-                .splineToLinearHeading(new Pose2d(-54, -52, Math.toRadians(225)), -1)
-                .waitSeconds(BASKET)
-                .strafeToLinearHeading(new Vector2d(-58, -35), Math.toRadians(270))
-                .waitSeconds(INTAKE)
-                .splineToLinearHeading(new Pose2d(-54, -52, Math.toRadians(225)), -1)
-                .waitSeconds(BASKET)
-                .strafeToLinearHeading(new Vector2d(-57, -33), Math.toRadians(345))
-                .waitSeconds(INTAKE)
-                .splineToLinearHeading(new Pose2d(-54, -52, Math.toRadians(225)), -1)
-                .waitSeconds(BASKET)
-                .strafeToLinearHeading(new Vector2d(-26, -12), Math.toRadians(180))
-                .waitSeconds(INTAKE + 1)
-                .splineToLinearHeading(new Pose2d(-54, -52, Math.toRadians(225)), -1)
-                .waitSeconds(BASKET)
-                .strafeToLinearHeading(new Vector2d(-26, -12), Math.toRadians(180))
+        redDrive.runAction(redDrive.getDrive().actionBuilder(new Pose2d(-13, -59, Math.toRadians(0)))
+                .strafeToLinearHeading(new Vector2d(-53, -53), Math.toRadians(45))
+                .strafeToLinearHeading(new Vector2d(-57,-47), Math.toRadians(60))
+                .strafeToLinearHeading(new Vector2d(-52, -51), Math.toRadians(45))
+                .splineToLinearHeading(new Pose2d(-40,-37, Math.toRadians(155)),1)
+                .splineToLinearHeading(new Pose2d(-51,-52, Math.toRadians(45)),1)
+                .splineToLinearHeading(new Pose2d(-52,-36, Math.toRadians(155)),1)
+                .splineToLinearHeading(new Pose2d(-51,-52, Math.toRadians(45)),1)
+                .splineToLinearHeading(new Pose2d(-28,-10, Math.toRadians(90)),1)
                 .build());
 
-        blueDrive.runAction(redDrive.getDrive().actionBuilder(new Pose2d(-13, -59, Math.toRadians(90)))
-                .strafeTo(new Vector2d(-45, -59))
-                .waitSeconds(1)
-                .strafeTo(new Vector2d(56, -59))
-                .build());
 
 
         meepMeep.setBackground(MeepMeep.Background.FIELD_INTO_THE_DEEP_JUICE_DARK)
                 .setDarkMode(true)
                 .setBackgroundAlpha(0.95f)
                 .addEntity(redDrive)
-                .addEntity(blueDrive)
                 .start();
     }
 }
